unexpectedjs/unexpected-mitm

commit 826e8960f6f1dfd0b15781e6ea236841fe123581

Use messy.Message.hasTextualContentType (available since messy 4.0.0) instead of including a separate implementation.

lib/unexpectedMitm.js
@@ -21,20 +21,6 @@ function formatHeaderObj(headerObj) {
     return result;
 }
 
-function isTextualContentType(contentType) {
-    if (typeof contentType === 'string') {
-        contentType = contentType.toLowerCase().trim().replace(/\s*;.*$/, '');
-        return (
-            /^text\//.test(contentType) ||
-            /^application\/(json|javascript)$/.test(contentType) ||
-            /^application\/xml/.test(contentType) ||
-            /^application\/x-www-form-urlencoded\b/.test(contentType) ||
-            /\+xml$/.test(contentType)
-        );
-    }
-    return false;
-}
-
 function bufferCanBeInterpretedAsUtf8(buffer) {
     // Hack: Since Buffer.prototype.toString('utf-8') is very forgiving, convert the buffer to a string
     // with percent-encoded octets, then see if decodeURIComponent accepts it.
@@ -52,7 +38,7 @@ function trimMessage(message) {
     if (typeof message.body !== 'undefined') {
         if (message.body.length === 0) {
             delete message.body;
-        } else if (isTextualContentType(message.headers['Content-Type']) && bufferCanBeInterpretedAsUtf8(message.body)) {
+        } else if (message.hasTextualContentType && bufferCanBeInterpretedAsUtf8(message.body)) {
             message.body = message.body.toString('utf-8');
         }
         if (/^application\/json(?:;|$)/.test(message.headers['Content-Type']) && /^\s*[[{]/.test(message.body)) {

commit 9aa8bd40baab6a69e905aba40052d828ba7e0b98

Fixed bug introduced in 826e896

lib/unexpectedMitm.js
@@ -38,7 +38,7 @@ function trimMessage(message) {
     if (typeof message.body !== 'undefined') {
         if (message.body.length === 0) {
             delete message.body;
-        } else if (message.hasTextualContentType && bufferCanBeInterpretedAsUtf8(message.body)) {
+        } else if (new messy.Message({headers: {'Content-Type': message.headers['Content-Type']}}).hasTextualContentType && bufferCanBeInterpretedAsUtf8(message.body)) {
             message.body = message.body.toString('utf-8');
         }
         if (/^application\/json(?:;|$)/.test(message.headers['Content-Type']) && /^\s*[[{]/.test(message.body)) {