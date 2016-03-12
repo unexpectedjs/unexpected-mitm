0b05eb752ee8ce9f6ad81011a3a31a373f981bae
Andreas Lind <andreas@one.com>
2016-03-12 22:18:54 +0100

    Removed describe/it mocks that weren't really needed.

--- bootstrap-unexpected-markdown.js
+++ bootstrap-unexpected-markdown.js
@@ -1,12 +1,4 @@
-/*global unexpected:true, describe:true, it:true*/
+/*global unexpected:true*/
 unexpected = require('unexpected');
 unexpected.output.preferredWidth = 80;
 unexpected.installPlugin(require('./lib/unexpectedMitm'));
-
-// Poor mans describe/it functions for the doc tests:
-
-it = describe = function (title, fn) {
-    if (fn) {
-        fn();
-    }
-};
