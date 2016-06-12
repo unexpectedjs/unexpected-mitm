0e10fc0f036355160cef195e4ad73f21f7d39abd
Alex J Burke <alex@alexjeffburke.com>
2016-06-12 17:08:09 +0100

    Add an explicit assertion mode flag which causes the return of the test file.
    This restores resaolving the the bare fulfilmentValue by default while still
    allowing opt-in to being returned the output file to be used when chaining.

--- test/unexpectedMitm.js
+++ test/unexpectedMitm.js
@@ -1618,7 +1618,7 @@ describe('unexpectedMitm', function () {
                     'Content-Type': 'application/x-www-form-urlencoded'
                 },
                 body: 'foo=bar'
-            }, 'with http mocked out', outputFile, 'to yield response', 405).finally(function () {
+            }, 'with http mocked out returning the file', outputFile, 'to yield response', 405).finally(function () {
                 delete process.env.UNEXPECTED_MITM_WRITE;
             });
         });
@@ -1637,7 +1637,7 @@ describe('unexpectedMitm', function () {
                 }
             }, 'was read correctly on', {
                 url: 'GET /'
-            }, 'with http mocked out', inputFile, 'to yield response', 405);
+            }, 'with http mocked out returning the file', inputFile, 'to yield response', 405);
         });
     });
 
