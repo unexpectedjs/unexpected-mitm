17ee68d0f31600552448ea77c595f7e1f49aee64
Andreas Lind <andreas@one.com>
2017-03-06 21:38:39 +0100

    Tests & documentation: Only rely on plugins that are explicitly installed
    Previously unexpected-messy and unexpected-http were used in places where
    it only worked because of bad isolation from other tests.

--- bootstrap-unexpected-markdown.js
+++ bootstrap-unexpected-markdown.js
@@ -1,4 +1,6 @@
 /*global unexpected:true*/
-unexpected = require('unexpected');
+unexpected = require('unexpected').clone();
 unexpected.output.preferredWidth = 80;
 unexpected.installPlugin(require('./lib/unexpectedMitm'));
+unexpected.installPlugin(require('unexpected-http'));
+unexpected.installPlugin(require('unexpected-express'));
