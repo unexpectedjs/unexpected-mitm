1c7acb69185ec96a97ae9d1bc32f76cdea97d994
Alex J Burke <alex@alexjeffburke.com>
2019-07-22 19:32:57 +0200

    Fix the minimal serialisation cases and create formatter tests.
    During the conversion that made the output recorded exchange
    formatter something that build the output up from messy objects
    these two cases did not read the correct source properties.
    
    Catch the errant cases by writing out tests and address them.

---
+++ test/formatRecordedExchange.js
@@ -0,0 +1,104 @@
+const expect = require('unexpected');
+const messy = require('messy');
+
+const formatRecordedExchange = require('../lib/formatRecordedExchange');
+
+describe('formatRecordedExchange', () => {
+  it('should serialise a minimal successful response to url property', () => {
+    const request = new messy.HttpRequest({ url: '/', method: 'POST' });
+    const exchange = new messy.HttpExchange({ request });
+
+    expect(formatRecordedExchange(exchange), 'to equal', {
+      request: 'POST /'
+    });
+  });
+
+  it('should serialise a minimal successful response to status code', () => {
+    const request = new messy.HttpRequest({ url: '/', method: 'GET' });
+    const response = new messy.HttpResponse({});
+    const exchange = new messy.HttpExchange({ request, response });
+
+    expect(formatRecordedExchange(exchange), 'to equal', {
+      request: 'GET /',
+      response: 200
+    });
+  });
+
+  it('should serialise a minimal arbitrary response', () => {
+    const request = new messy.HttpRequest({ url: '/', method: 'GET' });
+    const response = new messy.HttpResponse({ statusCode: 201 });
+    const exchange = new messy.HttpExchange({ request, response });
+
+    expect(formatRecordedExchange(exchange), 'to equal', {
+      request: 'GET /',
+      response: 201
+    });
+  });
+
+  it('should not serialise a 200 status code with other properties', () => {
+    const request = new messy.HttpRequest({ url: '/', method: 'GET' });
+    const response = new messy.HttpResponse({
+      statusCode: 200,
+      body: Buffer.from([0x66, 0x6f, 0x6f])
+    });
+    const exchange = new messy.HttpExchange({ request, response });
+
+    expect(formatRecordedExchange(exchange), 'to equal', {
+      request: 'GET /',
+      response: {
+        body: Buffer.from([0x66, 0x6f, 0x6f])
+      }
+    });
+  });
+
+  it('should serialise any other status code with other properties', () => {
+    const request = new messy.HttpRequest({ url: '/', method: 'GET' });
+    const response = new messy.HttpResponse({
+      statusCode: 201,
+      body: Buffer.from([0x66, 0x6f, 0x6f])
+    });
+    const exchange = new messy.HttpExchange({ request, response });
+
+    expect(formatRecordedExchange(exchange), 'to equal', {
+      request: 'GET /',
+      response: {
+        statusCode: 201,
+        body: Buffer.from([0x66, 0x6f, 0x6f])
+      }
+    });
+  });
+
+  it('should serialise a textual body', () => {
+    const request = new messy.HttpRequest({ url: '/', method: 'GET' });
+    const response = new messy.HttpResponse({
+      headers: {
+        'Content-Type': 'text/plain'
+      },
+      body: Buffer.from([0x66, 0x6f, 0x6f])
+    });
+    const exchange = new messy.HttpExchange({ request, response });
+
+    expect(formatRecordedExchange(exchange), 'to equal', {
+      request: 'GET /',
+      response: {
+        headers: {
+          'Content-Type': 'text/plain'
+        },
+        body: 'foo'
+      }
+    });
+  });
+
+  it('should not serialise an empty body', () => {
+    const request = new messy.HttpRequest({ url: '/', method: 'GET' });
+    const response = new messy.HttpResponse({
+      body: Buffer.from([])
+    });
+    const exchange = new messy.HttpExchange({ request, response });
+
+    expect(formatRecordedExchange(exchange), 'to equal', {
+      request: 'GET /',
+      response: 200
+    });
+  });
+});
